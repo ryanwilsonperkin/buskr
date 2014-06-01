dc13a446fff4d519cb3313c6b9375a76ba295e39
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2014-05-31 20:44:28 -0400

    Use Django-form for profile page.

--- gryphon/views.py
+++ gryphon/views.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 
+from artist.forms import CreateArtistForm
 from artist.models import Artist
 
 def donate(request, id):
@@ -16,7 +17,21 @@ def landing_page(request):
     return render(request, 'landing_page.html')
 
 def profile(request):
-    return render(request, 'profile.html')
+    if request.method == 'POST':
+        form = CreateArtistForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            profession = form.cleaned_data['profession']
+            description = form.cleaned_data['description']
+            thank_you_message = form.cleaned_data['thank_you_message']
+            return HttpResponseRedirect('/thanks/')
+    else:
+        form = CreateArtistForm()
+
+    return render(request, 'profile.html', {
+        'form': form,
+    })
 
 def share(request, id):
     try:
